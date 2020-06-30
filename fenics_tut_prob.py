907b5e709631ab4bdc7af2f41458b136c3fe813d
Joe Dean <jpd62@cam.ac.uk>
2020-07-01 00:39:05 +0100

    Fix problem, solution now looks good

--- fenics_tut_prob.py
+++ fenics_tut_prob.py
@@ -4,7 +4,7 @@
 
 # Solves for the magnetic field of a copper wire in a vacuum
 
-# FIXME Why does B not go to zero at the boundary?
+# TODO H(div) elements for normal jumps?
 
 import pygmsh
 import numpy as np
@@ -28,9 +28,9 @@ geom = pygmsh.opencascade.Geometry()
 outer_disk = geom.add_disk([0.0, 0.0, 0.0], 1.0, char_length=0.25)
 inner_disk = geom.add_disk([0.0, 0.0, 0.0], 0.05, char_length=0.01)
 frags = geom.boolean_fragments([outer_disk], [inner_disk])
-# Outer
+# Mark outer as 1
 geom.add_raw_code("Physical Surface(1) = {3};")
-# Inner
+# Mark inner as 2
 geom.add_raw_code("Physical Surface(2) = {2};")
 # print(geom.get_code())
 pygmsh_mesh = pygmsh.generate_mesh(geom)
@@ -79,18 +79,20 @@ facets = locate_entities_boundary(mesh, 1, bound_marker)
 bdofs = locate_dofs_topological(V, 1, facets)
 bc = DirichletBC(u_bc, bdofs)
 
-mu = [1.26e-6, 4 * np.pi * 1e-7]
-
-dx = Measure("dx", subdomain_data=mat_mt)
+mu_vacuum = 4 * np.pi * 1e-7
+mu_copper = 1.26e-6
 
 J = Constant(mesh, 1.0)
 A_z = TrialFunction(V)
 v = TestFunction(V)
 
+# Vacuum marked as 1, copper wire marked as 2
+dx = Measure("dx", subdomain_data=mat_mt)
+
 # TODO Write as function / list comprehension / use loop
-a = (1 / mu[0]) * inner(grad(A_z), grad(v)) * dx(1) + \
-    (1 / mu[1]) * inner(grad(A_z), grad(v)) * dx(2)
-L = inner(J, v) * dx(1)
+a = (1 / mu_vacuum) * inner(grad(A_z), grad(v)) * dx(1) + \
+    (1 / mu_copper) * inner(grad(A_z), grad(v)) * dx(2)
+L = inner(J, v) * dx(2)
 
 
 A_z = Function(V)
