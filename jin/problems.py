41ac6081e6b4b6e90e60737d515cc3bbc76e959a
Joe Dean <jpd62@cam.ac.uk>
2020-09-18 15:39:58 +0100

    Get solver running

--- jin/problems.py
+++ jin/problems.py
@@ -5,18 +5,34 @@ from dolfinx import FunctionSpace, Function
 
 
 class Problem:
-    def __init__(self, mesh, cell_mt, facet_mt, k, alpha_dict, beta_dict):
+    def __init__(self, mesh, cell_mt, facet_mt, k, alpha_dict, beta_dict, f):
         self._mesh = mesh
         self._cell_mt = cell_mt
         self._facet_mt = facet_mt
         self._k = k
         self._alpha = self._create_dg0_func(alpha_dict)
         self._beta = self._create_dg0_func(beta_dict)
+        self._f = f
 
     def _create_dg0_func(self, dict):
         V = FunctionSpace(self._mesh, ("Discontinuous Lagrange", 0))
         v = Function(V)
 
         for i in range(V.dim):
-            v.setValueLocal(i, dict[self._cell_mt[i]])
+            v.vector.setValueLocal(i, dict[self._cell_mt.values[i]])
         return v
+
+    def get_mesh(self):
+        return self._mesh
+
+    def get_k(self):
+        return self._k
+
+    def get_alpha(self):
+        return self._alpha
+
+    def get_beta(self):
+        return self._beta
+
+    def get_f(self):
+        return self._f
--- jin/solver.py
+++ jin/solver.py
@@ -6,10 +6,55 @@
 
 from problems import Problem
 from meshes import create_unit_square_mesh
+import dolfinx
+from dolfinx import FunctionSpace, Function
+from dolfinx.mesh import locate_entities_boundary
+from dolfinx.fem import locate_dofs_topological, DirichletBC
+from ufl import TrialFunction, TestFunction, inner, grad, dx
+import numpy as np
+
+
+# TODO Remove
+def boundary_marker(x):
+    left = np.isclose(x[0], 0)
+    right = np.isclose(x[0], 1)
+    bottom = np.isclose(x[1], 0)
+    top = np.isclose(x[1], 1.1)
+
+    l_r = np.logical_or(left, right)
+    b_t = np.logical_or(bottom, top)
+    l_r_b_t = np.logical_or(l_r, b_t)
+    return l_r_b_t
+
+
+# TODO Remove
+def bound_cond(x):
+    # TODO Make this use meshtags
+    values = np.zeros((1, x.shape[1]))
+    return values
 
 
 def solve(problem):
-    pass
+    V = FunctionSpace(problem.get_mesh(), ("Lagrange", problem.get_k()))
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    # TODO Use meshtags
+    u_bc = Function(V)
+    u_bc.interpolate(bound_cond)
+    facets = locate_entities_boundary(mesh, 1, boundary_marker)
+    bdofs = locate_dofs_topological(V, 1, facets)
+    bc = DirichletBC(u_bc, bdofs)
+
+    a = inner(1 / problem.get_alpha() * grad(u), grad(v)) * dx \
+        + inner(problem.get_beta() * u, v) * dx
+    L = inner(problem.get_f(), v) * dx
+
+    u = Function(V)
+    dolfinx.solve(a == L, u, bc, petsc_options={"ksp_type": "preonly",
+                                                "pc_type": "lu"})
+    return u
 
 
 h = 0.1
@@ -17,5 +62,6 @@ mesh, cell_mt, facet_mt = create_unit_square_mesh(h)
 k = 1
 alpha_dict = {1: 1}
 beta_dict = {1: 0}
-problem = Problem(mesh, cell_mt, facet_mt, k, alpha_dict, beta_dict)
+f = 1
+problem = Problem(mesh, cell_mt, facet_mt, k, alpha_dict, beta_dict, f)
 u = solve(problem)
