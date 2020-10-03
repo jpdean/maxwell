e9fdf0a9f411858f24b091c1292645dd4389b72a
Joe Dean <jpd62@cam.ac.uk>
2020-10-03 18:22:10 +0100

    Tidy

--- nedelec/solver.py
+++ nedelec/solver.py
@@ -1,13 +1,10 @@
-# TODO Get 3D working
 # TODO Add references
 
-from dolfinx import (UnitSquareMesh, DirichletBC, Function,
-                     FunctionSpace, Constant, solve,
+from dolfinx import (Function,  FunctionSpace, solve,
                      UnitCubeMesh, VectorFunctionSpace)
 import numpy as np
 from mpi4py import MPI
-from dolfinx.mesh import locate_entities_boundary
-from dolfinx.fem import locate_dofs_topological, assemble_scalar
+from dolfinx.fem import assemble_scalar
 from ufl import (TrialFunction, TestFunction, inner, dx, curl,
                  SpatialCoordinate, sin, cos, pi, as_vector)
 from dolfinx.io import XDMFFile
@@ -23,8 +20,7 @@ def project(f, V):
     L = inner(f, v) * dx
 
     u = Function(V)
-    solve(a == L, u, [], petsc_options={"ksp_type": "preonly",
-                                        "pc_type": "lu"})
+    solve(a == L, u, [], petsc_options={"ksp_type": "cg"})
     return u
 
 
@@ -41,22 +37,8 @@ def L2_norm(v):
                                             op=MPI.SUM))
 
 
-def solve_problem(k, mesh, T_0, bound_marker):
+def solve_problem(k, mesh, T_0):
     V = FunctionSpace(mesh, ("N1curl", k))
-
-    # Create function and set all dofs to 0
-    # A_d = Function(V)
-    # A_d.vector.set(0.0)
-    # # Locate boundary facets
-    # d = mesh.topology.dim
-    # facets = locate_entities_boundary(mesh, d - 1, bound_marker)
-    # # Locate boundary dofs
-    # dofs = locate_dofs_topological(V, d - 1, facets)
-    # # Apply boundary condition. NOTE Since these are edge elements,
-    # # this means that we are setting the tangential component of
-    # # A to zero on the boundary. The normal component is not
-    # # constrained (look at the basis funcs to see this)
-    # bc = DirichletBC(A_d, dofs)
 
     A = TrialFunction(V)
     v = TestFunction(V)
@@ -69,7 +51,7 @@ def solve_problem(k, mesh, T_0, bound_marker):
     # i.e. curl(grad(\phi)) = 0 for any \phi, so for any A that is a solution,
     # A + grad(\phi) is also a solution. Hence, must use an iterative solver.
     solve(a == L, A, [], petsc_options={"ksp_type": "cg",
-                                        "pc_type": "jacobi",
+                                        "pc_type": "icc",
                                         "ksp_rtol": 1e-12,
                                         "ksp_monitor": None})
     return A
@@ -95,71 +77,10 @@ def compute_B(A, k, mesh):
     return B
 
 
-# FIXME Move boundary markers to more sensible place
-def square_bound_marker(x):
-    left = np.isclose(x[0], 0)
-    right = np.isclose(x[0], 1)
-    bottom = np.isclose(x[1], 0)
-    top = np.isclose(x[1], 1)
-
-    l_r = np.logical_or(left, right)
-    b_t = np.logical_or(bottom, top)
-
-    return np.logical_or(l_r, b_t)
-
-
-# FIXME Move boundary markers to more sensible place
-def cube_bound_marker(x):
-    left = np.isclose(x[0], 0)
-    right = np.isclose(x[0], 1)
-    bottom = np.isclose(x[1], 0)
-    top = np.isclose(x[1], 1)
-    back = np.isclose(x[2], 0)
-    front = np.isclose(x[2], 1)
-
-    l_r = np.logical_or(left, right)
-    b_t = np.logical_or(bottom, top)
-    b_f = np.logical_or(back, front)
-
-    l_r_b_t = np.logical_or(l_r, b_t)
-
-    return np.logical_or(l_r_b_t, b_f)
-
-
 # FIXME Get ufl to compute f and B from A for checking solution
 # TODO Make problem factory
 
-# 2D Problems
 # Problem 1
-# k = 1
-# n = 32
-# mesh = UnitSquareMesh(MPI.COMM_WORLD, n, n)
-# x = SpatialCoordinate(mesh)
-# T_0 = as_vector()
-# A = solve_problem(k, mesh, f, square_bound_marker)
-# save_function(A, mesh, "A.xdmf")
-# B = compute_B(A, k - 1, mesh)
-# save_function(B, mesh, "B.xdmf")
-# B_e = x[1] - 0.5
-# e = L2_norm(B - B_e)
-# print(f"L2-norm of error in B = {e}")
-
-# Problem 2
-# k = 1
-# n = 64
-# mesh = UnitSquareMesh(MPI.COMM_WORLD, n, n)
-# x = SpatialCoordinate(mesh)
-# f = as_vector((pi**2 * sin(x[1] * pi), 0))
-# A = solve_problem(k, mesh, f, square_bound_marker)
-# save_function(A, mesh, "A.xdmf")
-# B = compute_B(A, k - 1, mesh)
-# save_function(B, mesh, "B.xdmf")
-# B_e = - pi * cos(x[1] * pi)
-# e = L2_norm(B - B_e)
-# print(f"L2-norm of error in B = {e}")
-
-# 3D Problems
-# Problem 3
 k = 1
 n = 32
 mesh = UnitCubeMesh(MPI.COMM_WORLD, n, n, n)
@@ -167,7 +88,7 @@ x = SpatialCoordinate(mesh)
 T_0 = as_vector((0,
                  x[1] * (x[1] - 1) * (2 * x[2] - 1),
                  x[2] * (1 - 2 * x[1]) * (x[2] - 1)))
-A = solve_problem(k, mesh, T_0, cube_bound_marker)
+A = solve_problem(k, mesh, T_0)
 save_function(A, mesh, "A.xdmf")
 B = compute_B(A, k - 1, mesh)
 save_function(B, mesh, "B.xdmf")
@@ -177,7 +98,7 @@ B_e = as_vector((0,
 e = L2_norm(B - B_e)
 print(f"L2-norm of error in B = {e}")
 
-# Problem 4
+# Problem 2
 # k = 1
 # n = 32
 # mesh = UnitCubeMesh(MPI.COMM_WORLD, n, n, n)
