3c8a086bb40367c17c720fcf80eff1932a9bd7bc
Joe Dean <jpd62@cam.ac.uk>
2020-10-13 14:10:05 +0100

    Add TODOs

--- nedelec/solver.py
+++ nedelec/solver.py
@@ -7,6 +7,7 @@ from util import project
 
 
 def solve_problem(problem):
+    # TODO Currently assumes homogeneous Neumann BCs
     V = FunctionSpace(problem.mesh, ("N1curl", problem.k))
 
     A = TrialFunction(V)
@@ -22,6 +23,8 @@ def solve_problem(problem):
     # NOTE That A is not unique because of the nullspace of the curl operator
     # i.e. curl(grad(\phi)) = 0 for any \phi, so for any A that is a solution,
     # A + grad(\phi) is also a solution. Hence, must use an iterative solver.
+    # TODO Set up solver manually
+    # TODO Get AMS working properly
     solve(a == L, A, [], petsc_options={"ksp_type": "cg",
                                         "pc_type": "icc",
                                         "ksp_rtol": 1e-12,
@@ -39,9 +42,6 @@ def compute_B(A, k, mesh):
     if mesh.topology.dim == 2:
         # Function space for B
         V = FunctionSpace(mesh, ("DG", k))
-        # B = curl(A), which in 2D is given by the formula here:
-        # https://www.khanacademy.org/math/multivariable-calculus/greens-theorem-and-stokes-theorem/formal-definitions-of-divergence-and-curl/a/defining-curl
-        # B is unique despite A not being
         B = project(A[1].dx(0) - A[0].dx(1), V)
     elif mesh.topology.dim == 3:
         V = VectorFunctionSpace(mesh, ("DG", k))
