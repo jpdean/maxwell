a0d8666327378f322794c5bc3785903be9a9b0b5
Joe Dean <jpd62@cam.ac.uk>
2021-01-05 17:06:09 +0000

    Add scripts to compute and plot interation results

---
+++ iterations.py
@@ -0,0 +1,34 @@
+from problems import create_problem_1
+from solver import solve_problem
+import numpy as np
+import pickle
+
+# Space degree
+k = 1
+# Permeability
+mu = 1
+# Initial element diameter
+hi = 0.25
+# Number of dofs ratio
+r = 2
+# Number of data points
+n = 7
+
+hs = [hi]
+for i in range(n):
+    hs.append(hs[-1] / np.cbrt(r))
+
+ndofs = []
+times = []
+iters = []
+for h in hs:
+    mesh, T_0, B_e = create_problem_1(h, mu)
+    results = solve_problem(mesh, k, mu, T_0)
+
+    ndofs.append(results["ndofs"])
+    times.append(results["solve_time"])
+    iters.append(results["iterations"])
+
+    # Write in each loop incase of crash etc.
+    results = [ndofs, times, iters]
+    pickle.dump(results, open("iteration_results.p", "wb"))
---
+++ plot_iterations.py
@@ -0,0 +1,19 @@
+import plotly.graph_objects as go
+import pickle
+import numpy as np
+
+save_figs_to_file = False
+
+ndofs, times, iters = pickle.load(open("iteration_results.p", "rb"))
+
+iter_fig = go.Figure(data=go.Scatter(x=ndofs, y=iters))
+iter_fig.update_layout(width=500,
+                       height=500,
+                       font=dict(size=20),
+                       xaxis=dict(title="NDOFs",
+                                  type="log",
+                                  exponentformat='power',
+                                  dtick=1),
+                       yaxis=dict(title="Iterations",
+                                  rangemode="tozero"))
+iter_fig.show()
