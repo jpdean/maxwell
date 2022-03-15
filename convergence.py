Modifications for new gradient operator. Minor changes to convergence script for more information output. Fix num dofs in parallel. Test convergence and iteration count for second order elements

convergence.py
@@ -2,6 +2,7 @@
 # have analytical solutions. Results are pickled to convergence_results.p
 # and can be plotted using plot_convergence.py.
 
+import numpy as np
 import pickle
 from solver import solve_problem
 import problems
@@ -10,24 +11,37 @@ from util import L2_norm
 # Problem
 create_problem = problems.create_problem_0
 # Characteristic element size
-hs = [1 / 4, 1 / 8, 1 / 16, 1 / 32]
+hs = [1 / 4, 1 / 8, 1 / 16, 1 / 32]  # 1 / 64]
 # Polynomial orders
-ks = [1]
+ks = [1, 2]
 # Coefficients
 alpha = 1.0
 beta = 1.0
 
-results = {}
+results = {k: [] for k in ks}
+iterations = {k: [] for k in ks}
+meshsize = {k: [] for k in ks}
+ndofs = {k: [] for k in ks}
 for k in ks:
-    l2_errors = []
     for h in hs:
         mesh, u_e, f, boundary_marker = create_problem(h, alpha, beta)
-        u = solve_problem(mesh, k, alpha, beta, f, boundary_marker, u_e)[0]
+        u, info = solve_problem(mesh, k, alpha, beta, f, boundary_marker, u_e)
         e = L2_norm(u - u_e)
-        l2_errors.append(e)
-    results[k] = (hs, l2_errors)
+        results[k].append(e)
+        meshsize[k].append(h)
+        iterations[k].append(info["iterations"])
+        ndofs[k].append(info["ndofs"])
 
-print(results)
+if mesh.comm.rank == 0:
+    for k in ks:
+        res = np.array(meshsize[k])
+        err = np.array(results[k])
+        print(f"k {k}")
+        print(f"Iterations {iterations[k]}")
+        print(f"Number of dofs {ndofs[k]}")
+        print(f"L2 error {err}")
+        print(f"Convergence rate {np.log(err[1:] / err[:-1]) / np.log(res[1:] / res[:-1])}")
+
 # Save results
 # TODO Use JSON instead
 pickle.dump(results, open("convergence_results.p", "wb"))

solver.py
@@ -96,8 +96,8 @@ def solve_problem(mesh: Mesh, k: int, alpha: np.float64, beta: np.float64,
 
         # Build discrete gradient
         V_CG = FunctionSpace(mesh, ("CG", k))._cpp_object
-        G = create_discrete_gradient(V._cpp_object, V_CG)
-
+        G = create_discrete_gradient(V_CG, V._cpp_object)
+        G.assemble()
         # Attach discrete gradient to preconditioner
         pc.setHYPREDiscreteGradient(G)
 
@@ -143,6 +143,6 @@ def solve_problem(mesh: Mesh, k: int, alpha: np.float64, beta: np.float64,
     u.x.scatter_forward()
     ksp.view()
     t.stop()
-    return (u, {"ndofs": u.vector.getSize(),
+    return (u, {"ndofs": V.dofmap.index_map.size_global * V.dofmap.index_map_bs,
                 "solve_time": t.elapsed()[0],
                 "iterations": ksp.its})